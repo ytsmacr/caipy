140bad3e8773b4bcf8789e9611db9c6550e09fd0
Cai <72549623+ytsmacr@users.noreply.github.com>
2023-01-19 18:01:55 +0000

    Update spectral_regression_modelling.py
    remove unused module r2_score

--- spectral_regression_modelling.py
+++ spectral_regression_modelling.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 from math import sqrt
 import warnings
